d9d21cf1085abee9aab8c0ac0e883053855dacf7
Shan Mi <ualarm@yahoo.com>
2020-10-28 01:41:04 -0400

    'style_update'

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,19 +2,19 @@ require("dotenv").config();
 
 module.exports = {
   siteMetadata: {
-    siteName: 'My Shop',
+    siteName: 'My Tea Shop',
   },
   plugins: [
     'gatsby-plugin-sass',
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-source-datocms`,
-      options: { apiToken: process.env.DATO_API_TOKEN },
+      options: { apiToken: '171431a7c788b523edd8115261de4f' },
     },
     {
       resolve: 'gatsby-plugin-snipcart',
       options: {
-        apiKey: 'OWE3MmZmMjQtNTk3Yi00OThhLWEwMmUtZDY4ZWM4NzIwYzZiNjM2NjM0Mzc1NzE0MTUwNzI1',
+        apiKey: 'ZmQ4Yzg1Y2ItYWE3Yi00Yzk5LThiNzgtYjQ4ZmM4MTc3MjIyNjM3Mzg1OTcyNTg3Mzk1Mzcz',
         autopop: true
       }
     },
--- src/pages/index.js
+++ src/pages/index.js
@@ -29,37 +29,39 @@ export default () => (
         }
       }
     `}
-render={data => (
-  <Layout site={data.site}>
-    <div className="Catalogue">
-      {
-        data.products.edges.map(({ node: product }) => (
-          <div className="Catalogue__item" key={product.id}>
-            <div
-              className="Product snipcart-add-item"
-              data-item-id={product.id}
-              data-item-price={product.price}
-              data-item-image={product.image.url}
-              data-item-name={product.name}
-              data-item-url={`/`}
-            >
-              <div className="Product__image">
-                <Img sizes={product.image.sizes} />
-              </div> <div className="Product__details">
-                <div className="Product__name">
-                  {product.name}
-                  <div className="Product__price">
-                    {product.price}€
+    render={data => (
+      <Layout site={data.site}>
+        <div className="Catalogue">
+          {
+            data.products.edges.map(({ node: product }) => (
+              <div className="Catalogue__item" key={product.id}>
+                <div>
+                  <div className="Product__image">
+                    <Img sizes={product.image.sizes} />
+                  </div> <div className="Product__details">
+                    <div className="Product__name">
+                      {product.name}
+                      <div className="Product__price">
+                        ${product.price}
+                      </div>
+                    </div>
+                    <button
+                      className="Product__buy Product snipcart-add-item"
+                      data-item-id={product.id}
+                      data-item-price={product.price}
+                      data-item-image={product.image.url}
+                      data-item-name={product.name}
+                      data-item-url={`https://determined-easley-e806d0.netlify.app/`}
+                    >
+                      Add to cart
+                    </button>
                   </div>
                 </div>
-                <span className="Product__buy">Buy now</span>
               </div>
-            </div>
-          </div>
-        ))
-      }
-    </div>
-  </Layout>
-     )}
+            ))
+          }
+        </div>
+      </Layout>
+    )}
    />
 )
